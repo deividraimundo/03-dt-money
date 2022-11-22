dd0ffaa5fda9f9367187bd5b03c887fdafb020c4
Deivid de Oliveira Raimundo <deivid.raimundo@aswti.com.br>
2022-11-22 09:25:28 -0300

    utilizando a memorização do react para evitar renderização supérflua

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -3,8 +3,26 @@ import { SearchFormContainer } from './styles'
 import { useForm } from 'react-hook-form'
 import * as z from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
-import { useContext } from 'react'
 import { TransactionsContext } from '../../../../contexts/TransactionsContext'
+import { useContextSelector } from 'use-context-selector'
+
+/**
+ * Por que um componente renderiza?
+ *
+ * - Hooks changed (mudou estado, contexto, reducer);
+ * - Props changed (mudou propriedades);
+ * - Parent rerendered (componente pai renderizou).
+ *
+ * Qual o fluxo de renderização?
+ *  1. O React recria o HTML da interface daquele componente;
+ *  2. Compara a versão do HTML recriada com a versão anterior;
+ *  3. Se mudou alguma coisa, ele reescreve o HTML na tela.
+ *
+ *  Memo:
+ *  0. Hooks changed, Props changed (deep comparison);
+ *  0.1 Comparar a versão anterior dos hooks e props
+ *  0.2 Se mudou algo, ele vai permitir a nova renderização
+ */
 
 const searchFormSchema = z.object({
   query: z.string(),
@@ -13,7 +31,12 @@ const searchFormSchema = z.object({
 type SearchFormInputs = z.infer<typeof searchFormSchema>
 
 export function SearchForm() {
-  const { fetchTransactions } = useContext(TransactionsContext)
+  const fetchTransactions = useContextSelector(
+    TransactionsContext,
+    (context) => {
+      return context.fetchTransactions
+    },
+  )
 
   const {
     register,
